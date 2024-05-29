32bdda5fb6966badbd9de430308d94c83587f0d4
botvi <younoob07mei@gmail.com>
2024-05-29 16:02:32 +0700

    Membuat Admin Dashboard dan Table Kategori,Berita

--- src/pagesAdmin/Kategori.jsx
+++ src/pagesAdmin/Kategori.jsx
@@ -134,24 +134,80 @@ const Kategori = () => {
           </div>
         </div>
         {/* Pagination */}
-        <nav className="block">
-          <ul className="flex pl-0 rounded list-none flex-wrap">
+        <nav aria-label="Page navigation example" className="mt-5">
+          <ul className="flex items-center -space-x-px h-8 text-sm">
+            <li>
+              <button
+                onClick={() => paginate(currentPage - 1)}
+                disabled={currentPage === 1}
+                className={`flex items-center justify-center px-3 h-8 ms-0 leading-tight text-gray-500 bg-white border border-e-0 border-gray-300 rounded-s-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white ${
+                  currentPage === 1 ? "cursor-not-allowed opacity-50" : ""
+                }`}
+              >
+                <span className="sr-only">Previous</span>
+                <svg
+                  className="w-2.5 h-2.5 rtl:rotate-180"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 6 10"
+                >
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M5 1 1 5l4 4"
+                  />
+                </svg>
+              </button>
+            </li>
             {Array.from({
               length: Math.ceil(categories.length / itemsPerPage),
             }).map((_, index) => (
               <li key={index}>
                 <button
                   onClick={() => paginate(index + 1)}
-                  className={`${
+                  className={`flex items-center justify-center px-3 h-8 leading-tight border border-gray-300 hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white ${
                     currentPage === index + 1
-                      ? "bg-blue-500 text-white hover:bg-blue-700"
-                      : "text-blue-500 hover:bg-blue-700 hover:text-white"
-                  } font-semibold py-2 px-4 border border-gray-300`}
+                      ? "z-10 text-blue-600 border-blue-300 bg-blue-50 hover:bg-blue-100 hover:text-blue-700 dark:bg-gray-700 dark:text-white"
+                      : "text-gray-500 bg-white"
+                  }`}
                 >
                   {index + 1}
                 </button>
               </li>
             ))}
+            <li>
+              <button
+                onClick={() => paginate(currentPage + 1)}
+                disabled={
+                  currentPage === Math.ceil(categories.length / itemsPerPage)
+                }
+                className={`flex items-center justify-center px-3 h-8 leading-tight text-gray-500 bg-white border border-gray-300 rounded-e-lg hover:bg-gray-100 hover:text-gray-700 dark:bg-gray-800 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white ${
+                  currentPage === Math.ceil(categories.length / itemsPerPage)
+                    ? "cursor-not-allowed opacity-50"
+                    : ""
+                }`}
+              >
+                <span className="sr-only">Next</span>
+                <svg
+                  className="w-2.5 h-2.5 rtl:rotate-180"
+                  aria-hidden="true"
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 6 10"
+                >
+                  <path
+                    stroke="currentColor"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="m1 9 4-4-4-4"
+                  />
+                </svg>
+              </button>
+            </li>
           </ul>
         </nav>
       </div>
