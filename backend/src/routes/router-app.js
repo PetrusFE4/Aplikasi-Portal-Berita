57f164df845da0dd974c4d2be0573cbe8802f22a
alcztn <alcztn@pm.me>
2024-06-16 11:09:02 +0700

    backend: Smol update; Fix some bugs

--- backend/src/routes/router-app.js
+++ backend/src/routes/router-app.js
@@ -6,8 +6,6 @@ const authController = require('../controllers/authController');
 const userController = require('../controllers/userController');
 const jwtAuth = require('../middlewares/jwt-auth');
 const authMiddleware = require('../middlewares/authMiddleware');
-const homeController = require('../controllers/controller-home');
-const profileController = require('../controllers/controller-profile');
 const newsController = require('../controllers/newsController');
 const favoriteController = require('../controllers/favoriteController');
 const commentController = require('../controllers/commentController');
@@ -20,17 +18,18 @@ router.post('/auth/signup', authController.signup);
 router.post('/auth/signin', authController.signin);
 
 router.get('/users', jwtAuth.verifyToken, userController.getAllUsers);
-router.get('/users/:userId', jwtAuth.verifyToken, userController.getUser);
+router.get('/users/:userId', userController.getUser);
 router.put('/users/:userId', jwtAuth.verifyToken, userController.updateUser);
 router.delete('/users/:userId', jwtAuth.verifyToken, userController.deleteUser);
 
 //router.get('/news', jwtAuth.verifyToken, newsController.getAllNews); // Perubahan disini
 router.get('/news', newsController.getAllNews);
+router.get('/news/:id', newsController.getNewsById);
 
 router.post('/news', jwtAuth.verifyToken, newsController.addNews);
 router.put('/news/:id', jwtAuth.verifyToken, newsController.updateNews);
 router.delete('/news/:id', jwtAuth.verifyToken, newsController.deleteNews);
-router.get('/news/category/:id_category', jwtAuth.verifyToken, newsController.getNewsByCategory); // Perubahan disini
+router.get('/news/category/:id_category', newsController.getNewsByCategory);
 
 
 router.get('/categories', categoryController.getAllCategories);
